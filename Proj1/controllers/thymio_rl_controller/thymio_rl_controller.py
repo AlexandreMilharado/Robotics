fb5376040f02d4a6483116b9832aeb873ba29d01
rsscp <rsscpereira@gmail.com>
2025-06-08 00:58:40 +0100

    Ola?

--- Proj1/controllers/thymio_rl_controller/thymio_rl_controller.py
+++ Proj1/controllers/thymio_rl_controller/thymio_rl_controller.py
@@ -26,12 +26,12 @@ try:
 except ImportError:
     sys.exit('Please make sure you have all dependencies installed.')
 
-
-MODEL_PATH = "PPO_test_3.zip"
-CSV_PATH = "rollout_test_3.csv"
+MODEL_LOAD = "PPO_test_8.zip"
+MODEL_PATH = "PPO_test_8___4.zip"
+CSV_PATH = "rollout_test_8___4.csv"
 
 TIME_STEP = 5
-EPISODE_STEPS = 500
+EPISODE_STEPS = 256
 LEDGE_THRESHOLD = 100
 PROX_THRESHOLD = 0.9
 ACC_THRESHOLD = 2
@@ -41,22 +41,31 @@ OBSTACLES_NUMBER = 10
 OBSTACLES_MIN_RADIUS = 0.35
 OBSTACLES_MAX_RADIUS = 0.67
 
-GRID_RESOLUTION = 0.117/4
+GRID_RESOLUTION = 0.117 * 5
+VELOCITY_THRESHOLD = 2
+WHEEL_DIFF_THRESHOLD = 0.1
 
-ROLLOUT_STEPS = EPISODE_STEPS * 4
-TOTAL_TIMESTEPS = ROLLOUT_STEPS * 250
-BATCH_SIZE = 64
+ROLLOUT_STEPS = 1000
+TOTAL_TIMESTEPS = 20000
+BATCH_SIZE = 16
 ENTROPY_COEFICIENT=0.005
 CLIP_RANGE=0.2
 VF_COEFICIENT=0.5
 LEARNING_RATE=3e-4
 MAX_GRAD_NORM=0.5
 
-REWARD_POSITVE_LINEAR_VELOCITY = 0.7
-REWARD_VISITED = 5
-PENALTY_PROX_OBSTACLES = 0.5
-PENALTY_LEDGE_OBSTACLES = 2
-PENALTY_LEDGE_FALL = 0.2
+# Com penalização a recuar da berma
+# Sem penalização around quando encontra obstáculo
+REWARD_GROUNDED_POSITVE_VELOCITY = 0
+VELOCITY_REWARD = 1
+REWARD_VISITED = 30
+PENALTY_BACKTRACK = 0
+PENALTY_VISITED = 0
+PENALTY_PROX_OBSTACLES = 15
+PENALTY_LEDGE_OBSTACLES = 20
+PENALTY_LEDGE_FALL = 100
+PENALTY_TURN = 1
+PENALTY_STUCK = 0
 
 
 # Structure of a class to create an OpenAI Gym in Webots.
@@ -82,7 +91,7 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
         self.action_space = gym.spaces.Box(
             low=np.array([-1, -1]),
             high=np.array([1, 1]),
-            dtype=np.float64)
+            dtype=np.float32)                                   # MUDANÇA
 
         # Fill in according to Thymio's sensors
         # See: https://www.gymlibrary.dev/api/spaces/
@@ -95,9 +104,10 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
                         1,
                         1, 1,
                         1, 1]),
-            dtype=np.float64)
+            dtype=np.float32)                                   # MUDANÇA
 
         self.__n = 0
+        self.__nn = 0
         self.__max_n = max_episode_steps
 
     #
@@ -133,7 +143,7 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
             super().step(self.__timestep)
 
         # set the initial state vector to return
-        init_state = self._inital_obs()
+        init_state = self._get_obs()
 
         # aditional info
         # info = self._get_info()
@@ -147,6 +157,7 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
     def step(self, action):
 
         self.__n = self.__n + 1
+        self.__nn = self.__nn + 1
 
         # start by applying the action in the robot actuators
         # See how in Lab 1 code
@@ -162,13 +173,14 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
         self._wheel_stuck(action)
         terminated = self._determine_terminated()
         truncated = self._determine_truncated()
+        done = terminated or truncated
 
         # compute the reward that results from applying the action in the current state
         reward = self._get_reward(terminated)
         self.total_episode_reward += reward
 
         # aditional info
-        info = self._get_info(terminated or truncated)
+        info = self._get_info(done, self.state)
 
         return self.state.astype(np.float64), reward, terminated, truncated, info
     
@@ -177,45 +189,58 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
         state += self._get_normalize_h_sensors()
         state += self._get_normalize_g_sensors()
 
-        return np.array(state)
+        return np.array(state, dtype=np.float32)
     
-    def _get_info(self, done):
+    def _get_info(self, done, obs):
         info = {}
+        info['terminal_observation'] = obs
         if done:
             info['episode'] = {
                 'r': self.total_episode_reward,
-                'l': self.__n
+                'l': self.__n,
             }
         return info
 
     def _set_velocities(self, action):
         self.left_motor.setVelocity(max(min(action[0], 1), -1) * self.left_motor.getMaxVelocity() )
         self.right_motor.setVelocity(max(min(action[1], 1), -1) * self.right_motor.getMaxVelocity())
+        #self.left_motor.setVelocity(2)
+        #self.right_motor.setVelocity(5)
 
     def _act(self):
         for i in range(10):
             super().step(self.__timestep)
 
+    def _get_info_vs(self, done):
+        frontal_readings = self._get_normalize_h_sensors()
+        ground_readings = self._get_normalize_g_sensors()
+
+        info = {}
+        if done:
+            info = {
+                'exploration': self._reward_exploration(),
+                'obstacles': self._penalty_prox_obstacles(frontal_readings),
+                'ledge': self._penalty_ledge(ground_readings)
+            }
+
+        return info
+
     def _get_reward(self, done):
+        frontal_readings = self._get_normalize_h_sensors()
+        
         total = 0
         total += self._reward_exploration()
-        total += self._penalty_prox_obstacles(self._get_normalize_h_sensors())
+        total += self._penalty_prox_obstacles(frontal_readings)
         total += self._penalty_ledge(self._get_normalize_g_sensors())
-        total += self._reward_linear_positive_velocity()
+        total += self._reward_positive_velocity()
+        total += self._reward_linear_velocity(frontal_readings)
+        total += self._penalty_backtrack()
         total += self._penalty_fall(total, done)
-        return total
+        #total += self._penalty_stuck()
 
-    def _determine_terminated(self):
-        fall = (
-            self.getFromDef("ROBOT").getField("translation").getSFVec3f()[2] < 0
-        )
+        #print("Reward: ", total)
 
-        stuck = self.stuck_counter > 30
-
-        terminated = stuck or fall
-        if(terminated):
-            print(f"--- EPISODE ENDED ---")
-        return terminated
+        return float(total)
 
     
     def _wheel_stuck(self, action):
@@ -236,6 +261,17 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
             self.stuck_counter = 0
         
         return stuck
+    
+
+    def _determine_terminated(self):
+        fall = (self.getFromDef("ROBOT").getField("translation").getSFVec3f()[2] < 0.9)
+        stuck = self.stuck_counter > 30
+        terminated = fall
+
+        if(terminated):
+            print(f"--- EPISODE ENDED ---")
+        return terminated
+
 
     def _determine_truncated(self):
         return self.__n > self.__max_n
@@ -345,30 +381,33 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
         gx = int(pos[0] / GRID_RESOLUTION)
         gy = int(pos[1] / GRID_RESOLUTION)
         return (gx, gy)
-    
-    def _reward_linear_positive_velocity(self):
+
+    def _reward_positive_velocity(self):
         left_vel = self.left_motor.getVelocity()
         right_vel = self.right_motor.getVelocity()
-
         both_forward = left_vel > 0 and right_vel > 0
-        aligned = abs(left_vel - right_vel) < 0.05
 
         gs = self._get_normalize_g_sensors()
         on_ground = gs[0] != 0 and gs[1] != 0
 
-        if both_forward and aligned and on_ground:
-            return REWARD_POSITVE_LINEAR_VELOCITY
-        elif not both_forward and on_ground:
-            return -REWARD_POSITVE_LINEAR_VELOCITY
-        else:
-            return 0
+        return REWARD_GROUNDED_POSITVE_VELOCITY * int(both_forward and on_ground)
     
+    def _penalty_stuck(self):
+        stuck = self.stuck_counter > 30
+        return PENALTY_STUCK * int(stuck)
+
+    def _penalty_backtrack(self):
+        left_vel = self.left_motor.getVelocity()
+        right_vel = self.right_motor.getVelocity()
+        both_back = left_vel < 0 and right_vel < 0
+        return -PENALTY_BACKTRACK * int(both_back)
+
     def _penalty_prox_obstacles(self, frontal_readings):
         return sum([-PENALTY_PROX_OBSTACLES * reading for reading in frontal_readings])
     
     def _penalty_ledge(self, ground_sensors):
         avg_vel = (self.left_motor.getVelocity() + self.right_motor.getVelocity()) / (2 * 9.53)
-        return sum([-PENALTY_LEDGE_OBSTACLES * int(1 - reading) * int(avg_vel > 0) for reading in ground_sensors])
+        return sum([-PENALTY_LEDGE_OBSTACLES * int(1 - reading) for reading in ground_sensors])
 
     def _reward_exploration(self):
         x, y = self._get_position_on_grid()
@@ -377,12 +416,36 @@ class OpenAIGymEnvironment(Supervisor, gym.Env):
             return 0
 
         self.last_grid_position = (x, y)
-        self.visited.add((x, y))
+
+        if (x, y) in self.visited:
+            return -PENALTY_VISITED
+        else:
+            self.visited.add((x, y))
+            return REWARD_VISITED
+            
+    def _reward_linear_velocity(self, frontal_readings):
+        leftV = self.left_motor.getVelocity()
+        rightV = self.right_motor.getVelocity()
+        maxV = max(abs(leftV), abs(rightV))
+        diff = min(abs(leftV-rightV)/maxV, 1)
+        percentV = ((leftV + rightV)/2)/maxV
         
-        return REWARD_VISITED
+        thresholdPass = (leftV + rightV)/2 > VELOCITY_THRESHOLD
+        diffPass = diff < WHEEL_DIFF_THRESHOLD
+
+        obscured = sum([reading for reading in frontal_readings]) > 0
+
+        #print("-")
+        #print("Passed velocity:", (leftV + rightV)/2)
+        #print("Passed diff:", diff)
+        #print("Condition:", thresholdPass and diffPass)
+
+        #return VELOCITY_REWARD * int(thresholdPass and diffPass) - PENALTY_AROUND * int(not diffPass)
+        return VELOCITY_REWARD * percentV - PENALTY_TURN * diff
         
     def _penalty_fall(self, current_reward, done):
-        return -((PENALTY_LEDGE_FALL*100) + PENALTY_LEDGE_FALL * abs(self.total_episode_reward + current_reward)) * int(done)
+        return -PENALTY_LEDGE_FALL * int(done)
+        #return -((PENALTY_LEDGE_FALL*100) + PENALTY_LEDGE_FALL * abs(self.total_episode_reward + current_reward)) * int(done)
         
 
 class RolloutCSVLogger(BaseCallback):
@@ -398,6 +461,8 @@ class RolloutCSVLogger(BaseCallback):
         logger = self.model.logger
         name_to_value = getattr(logger, 'name_to_value', {})
 
+
+        print("Episode reward:", sum(self.training_env.envs[0].get_episode_rewards()))
         ep_rew_mean = sum(self.training_env.envs[0].get_episode_rewards())/len(self.training_env.envs[0].get_episode_rewards())
         ep_len_mean = sum(self.training_env.envs[0].get_episode_lengths())/len(self.training_env.envs[0].get_episode_lengths())
         data = {
@@ -451,51 +516,52 @@ def main():
     print("CUDA device count:", torch.cuda.device_count())
     print("CUDA device:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
 
-    if os.path.exists(MODEL_PATH):
-        model = PPO.load(MODEL_PATH, env, device="cpu")
+    if os.path.exists(MODEL_LOAD):
+        #model = PPO.load(MODEL_PATH, env, device="cpu")
         
-        # model = RecurrentPPO.load(MODEL_PATH, env, device="cuda:0" if torch.cuda.is_available() else "cpu")
+        model = RecurrentPPO.load(MODEL_LOAD, env, device="cuda:0" if torch.cuda.is_available() else "cpu")
     else:
-        model = PPO(
-            "MlpPolicy", env, device="cpu",
-            batch_size=BATCH_SIZE,
-            ent_coef=ENTROPY_COEFICIENT,
-            clip_range=CLIP_RANGE,
-            vf_coef=VF_COEFICIENT,
-            learning_rate=exp_schedule(LEARNING_RATE),
-            max_grad_norm=MAX_GRAD_NORM,
-            verbose=1
-        )
-        
-        # model = RecurrentPPO(
-        #    "MlpLstmPolicy", env, device="cuda:0" if torch.cuda.is_available() else "cpu",
+        #model = PPO(
+        #    "MlpPolicy", env, device="cpu",
         #    batch_size=BATCH_SIZE,
         #    ent_coef=ENTROPY_COEFICIENT,
         #    clip_range=CLIP_RANGE,
         #    vf_coef=VF_COEFICIENT,
-        #    learning_rate=LEARNING_RATE,
+        #    learning_rate=exp_schedule(LEARNING_RATE),
         #    max_grad_norm=MAX_GRAD_NORM,
         #    verbose=1
-        # )
+        #)
+        
+        model = RecurrentPPO(
+            "MlpLstmPolicy", env, device="cuda:0" if torch.cuda.is_available() else "cpu",
+            n_steps=ROLLOUT_STEPS,
+            batch_size=BATCH_SIZE,
+            ent_coef=ENTROPY_COEFICIENT,
+            clip_range=CLIP_RANGE,
+            vf_coef=VF_COEFICIENT,
+            learning_rate=LEARNING_RATE,
+            max_grad_norm=MAX_GRAD_NORM,
+            verbose=1
+        )
 
-        checkpoint_callback = CheckpointCallback(save_freq=TOTAL_TIMESTEPS/4, save_path='./models/')
-        csv_logger = RolloutCSVLogger(CSV_PATH)
+    checkpoint_callback = CheckpointCallback(save_freq=TOTAL_TIMESTEPS/4, save_path='./models/')
+    csv_logger = RolloutCSVLogger(CSV_PATH)
 
-        callback = CallbackList([checkpoint_callback, csv_logger])
-        model.learn(TOTAL_TIMESTEPS, callback=callback)
+    callback = CallbackList([checkpoint_callback, csv_logger])
+    model.learn(TOTAL_TIMESTEPS, callback=callback)
 
-        model.save(MODEL_PATH)
+    model.save(MODEL_PATH)
 
         ###
 
     # Code to load a model and run it
     # For the RecurrentPPO case, consult its documentation
-    # obs, _ = env.reset()  # Handle new reset return format
-    # for _ in range(100000):
-    #     action, _states = model.predict(obs)
-    #     obs, reward, terminated, truncated, info = env.step(action)  # Handle new step return format
-    #     if terminated or truncated:
-    #         obs, _ = env.reset()
+    #obs, _ = env.reset()  # Handle new reset return format
+    #for _ in range(100000):
+    #    action, _states = model.predict(obs)
+    #    obs, reward, terminated, truncated, info = env.step(action)  # Handle new step return format
+    #    if terminated or truncated:
+    #        obs, _ = env.reset()
         
 
 
